12ada5ad54aa3a9341a10d3e956b8f5ca01af630
alexyoliveira <cupofteaoficial@gmail.com>
2023-07-05 10:43:18 -0300

    req 2 done

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -14,13 +14,14 @@ def find_file_by_name(context, search_term, case_sensitive=True):
         return []
 
     found_files = []
+    search_term = search_term.lower() if not case_sensitive else search_term
 
     for path in context["all_files"]:
-        file_name = path.split("/")[-1]
-
-        if not case_sensitive:
-            file_name.lower()
-            search_term.lower()
+        file_name = (
+            path.split("/")[-1].lower()
+            if not case_sensitive
+            else path.split("/")[-1]
+        )
 
         if search_term in file_name:
             found_files.append(path)
